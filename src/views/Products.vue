a9bb28e5b746a9166230c449f1b1600cdf1e56d6
Yang <noriyang0215@gmail.com>
2023-02-24 13:07:09 +0800

    first commit

---
+++ src/views/Products.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="text-end">
+    <button class="btn btn-primary" type="button" @click="openModal(true)">新增產品</button>
+  </div>
+  <table class="table mt-4">
+    <thead>
+      <tr>
+        <th width="120">分類</th>
+        <th>產品名稱</th>
+        <th width="120">原價</th>
+        <th width="120">售價</th>
+        <th width="100">是否啟用</th>
+        <th width="200">編輯</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item of products" :key="item.id">
+        <td>{{ item.category }}</td>
+        <td>{{ item.title }}</td>
+        <td class="text-right">
+          {{ item.origin_price }}
+        </td>
+        <td class="text-right">
+          {{ item.price }}
+        </td>
+        <td>
+          <span v-if="item.is_enabled" class="text-success">啟用</span>
+          <span v-else class="text-muted">未啟用</span>
+        </td>
+        <td>
+          <div class="btn-group">
+            <button class="btn btn-outline-primary btn-sm" @click="openModal(false, item)">編輯</button>
+            <button class="btn btn-outline-danger btn-sm" @click="openDelModal(item)">刪除</button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <ProductModal ref="productModal" :product="tempProduct" @update-product="updateProduct"></ProductModal>
+  <DelModal :item="tempProduct" ref="delModal" @del-item="delProduct"/>
+</template>
+<script>
+import ProductModal from '@/components/ProductModal.vue'
+import DelModal from '@/components/DelModal.vue'
+export default {
+  data () {
+    return {
+      products: [],
+      pagination: {},
+      tempProduct: {},
+      isNaN: false
+    }
+  },
+  components: { ProductModal, DelModal },
+  methods: {
+    openModal (isNew, item) {
+      this.tempProduct = isNew ? {} : { ...item }
+      this.isNew = isNew
+      const productComponent = this.$refs.productModal
+      productComponent.showModal()
+    },
+    openDelModal (item) {
+      this.tempProduct = { ...item }
+      const delComponent = this.$refs.delModal
+      delComponent.showModal()
+    },
+    getProducts () {
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products`
+      this.$http.get(api)
+        .then((res) => {
+          if (res.data.success) {
+            this.products = res.data.products
+            this.pagination = res.data.pagination
+          }
+        })
+    },
+    updateProduct (item) {
+      this.tempProduct = item
+      // 新增
+      let api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product`
+      let httpMethod = 'post'
+      // 編輯
+      if (!this.isNew) {
+        api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product/${item.id}`
+        httpMethod = 'put'
+      }
+      const productComponent = this.$refs.productModal
+      this.$http[httpMethod](api, { data: this.tempProduct })
+        .then((res) => {
+          console.log('res', res)
+          productComponent.hideModal()
+          this.getProducts()
+        })
+    },
+    delProduct () {
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product/${this.tempProduct.id}`
+      this.$http.delete(api)
+        .then((res) => {
+          if (res.data.success) {
+            const delComponent = this.$refs.delModal
+            delComponent.hideModal()
+            this.getProducts()
+          }
+        })
+    }
+  },
+  created () {
+    this.getProducts()
+  }
+}
+</script>
